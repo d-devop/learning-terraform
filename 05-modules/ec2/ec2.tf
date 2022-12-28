ca711430dc896bf4df9db730a302c9f8a4018c3d
Divya <divya5guntaka@gmail.com>
2022-12-28 18:10:28 +0530

    Update learning-terraform

---
+++ 05-modules/ec2/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "test" {
+  ami                    = var.ami
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [var.sgid]
+}
+
+variable "sgid" {}
+variable "ami" {}
---
+++ 05-modules/main.tf
@@ -0,0 +1,15 @@
+module "sg" {
+  source = "./sg"
+}
+
+module "ec2" {
+  source = "./ec2"
+  sgid   = module.sg.sgid
+  ami    = "ami-0a017d8ceb274537d"
+}
+
+module "ec21" {
+  source = "./ec2"
+  sgid   = module.sg.sgid
+  ami    = "ami-0b5eea76982371e91"
+}
